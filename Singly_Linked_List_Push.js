3479fef441bd0da09e71aa065aa25ce3dd644bcb
Li Leo <Leo.Li@jeffco.k12.co.us>
2021-04-24 14:02:39 -0600

    singly linked list push

---
+++ Singly_Linked_List_Push.js
@@ -0,0 +1,36 @@
+// piece of data -val
+// reference to the next node - next
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+
+    }
+    push(val){
+        var valNode = new Node(val);
+        if(!this.head){
+            this.head = valNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = valNode;
+            this.tail = valNode;
+        }
+        this.length++;
+        // this will return the whole list
+        return this;
+    }
+}
+// var first = new Node("hi");
+// first.next = new Node("there");
+// first.next.next = new Node("3rd item");
+
+var list = new SinglyLinkedList();
